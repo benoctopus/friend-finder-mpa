44a0d5529d51048448d495252ee6de2444210f47
Benjamin <rose.benjamin925@gmail.com>
2018-05-22 01:29:15 -0700

    added all friends page

--- src/server/routes/htmlRoutes.js
+++ src/server/routes/htmlRoutes.js
@@ -4,7 +4,12 @@ const routes = require('express').Router();
 routes.get('/survey', (req, res) => res.render('survey', {questions}));
 
 routes.get('/allfriends', (req, res) => {
-  res.json(require('../data/friends').get())
+  res.render(
+    'allFriends',
+    {
+    friends: require('../data/friends').get(),
+    alt: 'https://www.professionalclick.com/images/default.jpg'
+  })
 });
 
 
